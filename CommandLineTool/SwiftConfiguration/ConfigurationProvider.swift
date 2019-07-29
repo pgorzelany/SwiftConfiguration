4c097948f9b0f3668715a590e2a5b87af28e6df5
pgorzelany <pgorzelany@objectivity.co.uk>
2019-07-29 16:32:12 +0200

    Small refactor

--- CommandLineTool/SwiftConfiguration/ConfigurationProvider.swift
+++ CommandLineTool/SwiftConfiguration/ConfigurationProvider.swift
@@ -22,7 +22,7 @@ class ConfigurationProvider {
         return try configurationsDictionary.map { configurationDictionary -> Configuration in
             print(configurationDictionary)
             guard let name = configurationDictionary.key as? String, let contents = configurationDictionary.value as? Dictionary<String, Any> else {
-                throw ConfigurationError(message: "The configuration file has invalid format. Please consult the docs.")
+                throw ConfigurationError(message: "The configuration file has invalid format. Please refer to the docs.")
             }
 
             return Configuration(name: name, contents: contents)
